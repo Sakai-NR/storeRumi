af8d92cde3a8ac3dbfcb66ac9240a773b3ebceca
Noriaki Sakai <avengers3000ily@gmail.com>
2020-08-19 12:40:19 +0900

    Add closeKeybord

--- Top3 12.38.43/Top3/ShopView/SearchTableViewCell.swift
+++ Top3 12.38.43/Top3/ShopView/SearchTableViewCell.swift
@@ -18,15 +18,28 @@ extension SearchTableViewCellDelegate {
     
 }
 // MARK: - Property
-class SearchTableViewCell: BaseTableViewCell {
+class SearchTableViewCell: BaseTableViewCell ,UISearchBarDelegate{
     weak var delegate: SearchTableViewCellDelegate? = nil
+    @IBOutlet weak var shopSarchBar: UISearchBar!
 }
 
 // MARK: - Life cycle
 extension SearchTableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.shopSarchBar.endEditing(true)
+        shopSarchBar.delegate = self
     }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+//        print("end searching --> Close Keyboard")
+        self.shopSarchBar.endEditing(true)
+    }
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        self.shopSarchBar.endEditing(true)
+        shopSarchBar.resignFirstResponder()
+        return true
+    }
+    
 }
 
 // MARK: - Protocol
