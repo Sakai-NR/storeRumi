5a7c28d5b6fa4059fa95bc4ffcfecd66543e3085
Noriaki Sakai <avengers3000ily@gmail.com>
2020-08-26 11:43:03 +0900

    Add NewItemCell

---
+++ Top3 12.38.43/Top3/ShopView/NewItemTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  NewItemTableViewCell.swift
+//  Top3
+//
+//  Created by 酒井典昭 on 2020/08/25.
+//  Copyright © 2020 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+protocol NewItemTableViewCellDelegate: NSObjectProtocol{
+    
+}
+
+extension NewItemTableViewCellDelegate {
+    
+}
+// MARK: - Property
+class NewItemTableViewCell: BaseTableViewCell {
+    weak var delegate: NewItemTableViewCellDelegate? = nil
+    @IBOutlet weak var NewItemImageView: UIImageView!
+    @IBOutlet weak var explanationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+}
+
+// MARK: - Life cycle
+extension NewItemTableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
+// MARK: - Protocol
+extension NewItemTableViewCell {
+    
+}
+
+// MARK: - method
+extension NewItemTableViewCell {
+    
+}
+
